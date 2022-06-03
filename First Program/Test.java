8e466f50cddebc710e819dcf86f7fdeb11c25c44
Ayush <ayush.kanduri@gmail.com>
2022-06-03 20:32:24 +0530

    First Commit

---
+++ First Program/Test.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Test{
+public static void main(String[] args) {
+    int var;
+    System.out.println("Hello World");
+    Scanner sc= new Scanner(System.in);
+    var=sc.nextInt();
+    System.out.println("The value of var is "+var);
+    sc.close();
+}
+} 
